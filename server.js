f52f7146fbe0ecfd204e65957943c847b344d151
rishikumar-simform <rishikumar.vachhani@simformsolutions.com>
2025-03-18 14:03:55 +0530

    update readme.md

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD, // Change to your MySQL password
+    password: process.env.DB_PASSWORD, 
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
 });
